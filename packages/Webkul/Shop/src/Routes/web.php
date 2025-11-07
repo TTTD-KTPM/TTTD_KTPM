a0e984607d956bc978ef837802846539db44615e
Triet Do <trietdeptraingu@gmail.com>
2025-11-07 11:44:48 +0700

    Add profile pages for customer and test for profile pages

--- packages/Webkul/Shop/src/Routes/web.php
+++ packages/Webkul/Shop/src/Routes/web.php
@@ -15,4 +15,4 @@ require 'customer-routes.php';
  * Checkout routes. All routes related to checkout like
  * cart, coupons, etc will be placed here.
  */
-// require 'checkout-routes.php';
+require 'checkout-routes.php';
