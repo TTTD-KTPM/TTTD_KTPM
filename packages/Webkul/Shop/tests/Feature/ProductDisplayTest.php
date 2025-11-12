8418e85782e8c8316bef0cb357cd72972e980bc1
ToanTranDuc <146490130+ToanTranDuc@users.noreply.github.com>
2025-11-12 23:02:49 +0700

    test: Add ProductDisplayTest case 2 - Display product information

--- packages/Webkul/Shop/tests/Feature/ProductDisplayTest.php
+++ packages/Webkul/Shop/tests/Feature/ProductDisplayTest.php
@@ -17,3 +17,22 @@ it('should display product detail page for customers', function () {
         ->assertOk()
         ->assertSeeText($product->name);
 });
+
+it('should display product information correctly', function () {
+    // Arrange
+    $product = (new ProductFaker)->getSimpleProductFactory()->create([
+        'name'              => 'Test Product Display',
+        'price'             => 199.99,
+        'description'       => 'Detailed product description',
+        'short_description' => 'Short description here',
+        'status'            => 1,
+        'visible_individually' => 1,
+    ]);
+
+    // Act and Assert
+    get(route('shop.product_or_category.index', $product->url_key))
+        ->assertOk()
+        ->assertSeeText('Test Product Display')
+        ->assertSeeText('199.99')
+        ->assertSeeText('Detailed product description');
+});
